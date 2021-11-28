685b44505e365a8c9f75b7eedd85cb5333673fca
DanielaMazur <danielamazur2000@gmail.com>
2021-11-28 15:15:25 +0200

    Setup socket messaging

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -1,17 +1,42 @@
+import { useEffect, useRef, useState } from "react";
 import "./chat.styles.css";
+
+const webSocket = new WebSocket("wss://localhost:44324");
 
 type ChatProps = {
   userName: string;
 };
 
 const Chat = (props: ChatProps) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const [messages, setMessages] = useState<string[]>([]);
+
+  useEffect(() => {
+    webSocket.onmessage = (message) => {
+      setMessages((prevMessages) => [...prevMessages, message.data]);
+    };
+  });
+
+  const handleSendMessage = () => {
+    if (!inputRef.current) return;
+    webSocket.send(props.userName + ": " + inputRef.current.value);
+    inputRef.current.value = "";
+  };
+
   return (
     <div className="chat-page">
       <h3>Hi {props.userName}</h3>
       <div className="actions-container ">
-        <input className="chat-input" />
-        <button className="submit-button">Send</button>
+        <input className="chat-input" ref={inputRef} />
+        <button className="submit-button" onClick={handleSendMessage}>
+          Send
+        </button>
       </div>
+
+      {messages.map((message) => (
+        <div key={props.userName + message}>{message}</div>
+      ))}
     </div>
   );
 };
